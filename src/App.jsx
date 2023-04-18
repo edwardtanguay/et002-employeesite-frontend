8801e36258e1b27a7bd00fc01421f71914f3aed4
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-18 06:44:17 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Company Site</h1>
+      <p>This is the company site.</p>
+    </div>
+  );
+}
+
+export default App;
