658cb13f8d465dd4a2029d670d83e904cd857a97
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-18 07:15:13 +0200

    show employees number

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
 
+const backendUrl = 'http://localhost:3333';
+
 function App() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setEmployees((await axios.get(`${backendUrl}/employees`)).data);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <h1>Company Site</h1>
-      <p>This is the company site.</p>
+      <h2>Employees</h2>
+      <p>There are {employees.length} employees:</p>
     </div>
   );
 }
