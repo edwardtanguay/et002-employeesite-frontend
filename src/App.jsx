675dbf80f3c059d8f15461de251207af9f93f24d
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-18 07:28:10 +0200

    shows employees

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,32 @@ import './App.scss';
 const backendUrl = 'http://localhost:3333';
 
 function App() {
-  const [employees, setEmployees] = useState([]);
+	const [employees, setEmployees] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      setEmployees((await axios.get(`${backendUrl}/employees`)).data);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			setEmployees((await axios.get(`${backendUrl}/employees`)).data);
+		})();
+	}, []);
 
-  return (
-    <div className="App">
-      <h1>Company Site</h1>
-      <h2>Employees</h2>
-      <p>There are {employees.length} employees:</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Company Site</h1>
+			<h2>Employees</h2>
+			<p>There are {employees.length} employees:</p>
+			<div className="employees">
+				{employees.map(employee => {
+					return (
+						<div className="employee" key={employee.id}>
+							<div className="fullName">{employee.firstName} {employee.lastName}</div>
+							<div className="title">{employee.title}</div>
+							<div className="age">Age: {employee.age}</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
